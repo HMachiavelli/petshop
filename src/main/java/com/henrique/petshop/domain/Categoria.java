3270aef199a2a478f8e56d8623779db943f0d667
Henrique Machiavelli <hsmachiavelli@gmail.com>
2021-11-08 19:38:55 -0300

    feat: cria relacionamento categoria x produto

--- src/main/java/com/henrique/petshop/domain/Categoria.java
+++ src/main/java/com/henrique/petshop/domain/Categoria.java
@@ -1,12 +1,15 @@
 package com.henrique.petshop.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -17,6 +20,17 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
 	public Categoria() {}
 
 	public Categoria(Integer id, String nome) {
--- src/main/java/com/henrique/petshop/utils/Seeder.java
+++ src/main/java/com/henrique/petshop/utils/Seeder.java
@@ -8,12 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.henrique.petshop.domain.Categoria;
+import com.henrique.petshop.domain.Produto;
 import com.henrique.petshop.repositories.CategoriaRepository;
+import com.henrique.petshop.repositories.ProdutoRepository;
 
 @Component
 public class Seeder {
 	@Autowired
 	CategoriaRepository categoriaRepo;
+	@Autowired
+	ProdutoRepository produtoRepo;
 	
 	@PostConstruct
 	public void seed() {
@@ -21,6 +25,21 @@ public class Seeder {
 		Categoria cat2 = new Categoria(null, "Cosmético");
 		Categoria cat3 = new Categoria(null, "Remédio");
 		
+		Produto prd1 = new Produto(null, "Ração", 100.00);
+		Produto prd2 = new Produto(null, "Sache", 80.00);
+		Produto prd3 = new Produto(null, "Shampoo", 50.00);
+		Produto prd4 = new Produto(null, "Vermifugo", 20.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(prd1, prd2));
+		cat2.getProdutos().addAll(Arrays.asList(prd3));
+		cat3.getProdutos().addAll(Arrays.asList(prd3, prd4));
+		
+		prd1.getCategorias().addAll(Arrays.asList(cat1));
+		prd2.getCategorias().addAll(Arrays.asList(cat1));
+		prd3.getCategorias().addAll(Arrays.asList(cat2));
+		prd4.getCategorias().addAll(Arrays.asList(cat2, cat3));
+		
 		categoriaRepo.saveAll(Arrays.asList(cat1, cat2, cat3));
+		produtoRepo.saveAll(Arrays.asList(prd1, prd2, prd3, prd4));
 	}
 }

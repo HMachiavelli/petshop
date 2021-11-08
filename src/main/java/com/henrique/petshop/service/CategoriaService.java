91c02e8e499ad99ee42528986da17cbbdb70f897
Henrique Machiavelli <hsmachiavelli@gmail.com>
2021-11-08 19:54:49 -0300

    feat: tratamento de erros e referencias ciclicas

--- src/main/java/com/henrique/petshop/service/CategoriaService.java
+++ src/main/java/com/henrique/petshop/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.henrique.petshop.domain.Categoria;
 import com.henrique.petshop.repositories.CategoriaRepository;
+import com.henrique.petshop.service.exceptions.ObjetoNaoEncontradoException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,6 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjetoNaoEncontradoException("Objeto não encontrado. ID: " + id + ", Tipo: " + Categoria.class.getCanonicalName()));
 	}
 }
